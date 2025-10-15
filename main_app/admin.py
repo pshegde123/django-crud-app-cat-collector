158706116e2794c33abe7361f45d07a21ae889a1
pshegde123 <pradnyahegde@gmail.com>
2025-10-15 15:01:26 -0400

    added cat feedings model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Cat
+from .models import Cat, Feeding, Toy
 
 admin.site.register(Cat)
+admin.site.register(Feeding)
+admin.site.register(Toy)
